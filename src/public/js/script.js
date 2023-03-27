8f8aa1fa92281726f590914dad5672d27e144691
DucDien2505 <trieuducdien2505@gmail.com>
2023-03-27 15:28:45 +0700

    home signin signup ui

---
+++ src/public/js/script.js
@@ -0,0 +1,30 @@
+// Khi người dùng cuộn trang, kiểm tra vị trí và hiển thị nút khi cần thiết
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scrollToTopBtn").style.display = "block";
+  } else {
+    document.getElementById("scrollToTopBtn").style.display = "none";
+  }
+}
+
+//  cuộn trang về đầu trang
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
+
+// Lấy phần tử nút navbar-toggler và danh sách các nav-link
+const navbarToggler = document.querySelector(".navbar-toggler");
+const navLinks = document.querySelectorAll(".nav-link");
+
+// Thêm sự kiện click vào nút navbar-toggler
+navbarToggler.addEventListener("click", function() {
+    // Thêm lớp "selected" cho các nav-link được chọn
+    navLinks.forEach(function(navLink) {
+        navLink.classList.toggle("selected");
+    });
+});
+
+
