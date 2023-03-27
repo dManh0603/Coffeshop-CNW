9cdf37919b40e90936410acc581fe9e094408c22
Dat Tran <103311666+Datitneu@users.noreply.github.com>
2023-03-27 23:23:03 +0700

    About us

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,30 +1,34 @@
-const Product = require('../models/Product');
-const { multipleMongooseToObject, } = require('../../util/mongoose');
+const Product = require("../models/Product");
+const { multipleMongooseToObject } = require("../../util/mongoose");
 class SiteController {
+  // [GET] /
+  index(req, res, next) {
+    Product.find({})
+      .then((products) => {
+        res.render("site/home", {
+          title: "Home page",
+          products: multipleMongooseToObject(products),
+        });
+      })
+      .catch((e) => {
+        next(e);
+      });
+  }
 
-    // [GET] /
-    index(req, res, next) {
-        Product.find({})
-            .then(products => {
-                res.render('site/home', {
-                    title: 'Home page',
-                    products: multipleMongooseToObject(products),
-                });
-            })
-            .catch(e => {
-                next(e);
-            })
-    }
+  // [GET] /search
+  search(req, res) {
+    res.render("site/search");
+  }
 
-    // [GET] /search
-    search(req, res) {
-        res.render('site/search');
-    }
+  // [GET] /contact
+  contact(req, res) {
+    res.render("site/contact");
+  }
 
-    // [GET] /contact
-    contact(req, res) {
-        res.render('site/contact');
-    }
+  // [GET] /about
+  about(req, res) {
+    res.render("site/about");
+  }
 }
 
-module.exports = new SiteController;
+module.exports = new SiteController();
--- src/routes/site.js
+++ src/routes/site.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const siteController = require('../app/controllers/SiteController');
- 
-router.get('/search', siteController.search)
-router.get('/contact', siteController.contact)
-router.get('/', siteController.index)
+const siteController = require("../app/controllers/SiteController");
+
+router.get("/search", siteController.search);
+router.get("/about", siteController.about);
+router.get("/contact", siteController.contact);
+router.get("/", siteController.index);
 
 module.exports = router;
