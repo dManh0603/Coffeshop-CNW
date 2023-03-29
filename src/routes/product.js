c3e8f7139b61e4369b0c74a9ebe7ffefad331793
dmanh <dmanh6302@gmail.com>
2023-03-29 21:37:26 +0700

    Load product to menu

--- src/routes/product.js
+++ src/routes/product.js
@@ -16,16 +16,16 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 router.get('/create', productController.create)
+router.get('/:id/edit', productController.edit)
+router.get('/:slug', productController.show)
 router.post('/handle-form-actions', productController.handleFormActions)
 router.post('/store', upload.single('picture'), googleDrive.toGoogle, productController.store)
-router.get('/:id/edit', productController.edit)
 router.put('/:id', upload.single('picture'), googleDrive.toGoogle, productController.update, googleDrive.deleteFile, (req, res) =>
     res.redirect('/me/stored/products'));
 router.patch('/:id/restore', productController.restore)
 router.delete('/:id', productController.delete)
 router.delete('/:id/destroy', productController.destroy, googleDrive.deleteFile, (req, res) =>
     res.redirect('/me/stored/products'));
-router.get('/:slug', productController.show)
 
 
 module.exports = router;
