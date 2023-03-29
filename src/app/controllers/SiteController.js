2f411b09ba746a4321f4643490f0460435c6178a
Dat Tran <103311666+Datitneu@users.noreply.github.com>
2023-03-29 16:03:42 +0700

    Done thêm chi tiết sản phẩm

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -25,43 +25,43 @@ class SiteController {
     res.render("site/contact");
   }
 
-    // [GET] /contact
-    contact(req, res) {
-        res.render('site/contact');
-    }
-   
-     // [GET] /menu
-    menu(req, res) {
-        res.render('site/menu');
-    }
-     // [GET] /about
-    about(req, res) {
-        res.render('site/about');
-    }
-     // [GET] /signin
-    signin(req, res) {
-        res.render('site/signin');
-    }
-    // [GET] /signup
-    signup(req, res) {
-        res.render('site/signup');
-    }
-    // [GET] /signout
-    signout(req, res) {
-        res.render('site/signout');
-    }
-    // [GET] /home
-    home(req, res) {
-        res.render('site/home');
-    }
-    // [GET] /shop
-    shop(req, res) {
-        res.render('site/shop');
-    }
+  // [GET] /contact
+  contact(req, res) {
+    res.render("site/contact");
+  }
+
+  // [GET] /menu
+  menu(req, res) {
+    res.render("site/menu");
+  }
+  // [GET] /about
+  about(req, res) {
+    res.render("site/about");
+  }
+  // [GET] /signin
+  signin(req, res) {
+    res.render("site/signin");
+  }
+  // [GET] /signup
+  signup(req, res) {
+    res.render("site/signup");
+  }
+  // [GET] /signout
+  signout(req, res) {
+    res.render("site/signout");
+  }
+  // [GET] /home
+  home(req, res) {
+    res.render("site/home");
+  }
+  // [GET] /shop
+  shop(req, res) {
+    res.render("site/shop");
+  }
+  // [GET] /product_detail
+  product_detail(req, res) {
+    res.render("site/product_detail");
+  }
 }
 
-
-
-
-
-module.exports = new SiteController;
+module.exports = new SiteController();
