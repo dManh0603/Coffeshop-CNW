c1a016957466b1e72f6a899eec52cd668b5dd277
dmanh <dmanh6302@gmail.com>
2023-03-31 08:35:08 +0700

    load product after click add to cart

---
+++ src/app/models/Cart.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+const cartItemSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    default: 1
+  },
+  price: {
+    type: Number,
+    required: true
+  }
+});
+
+const cartSchema = new mongoose.Schema({
+  items: [cartItemSchema],
+  total: {
+    type: Number,
+    required: true,
+    default: 0
+  }
+});
+
+cartSchema.plugin(AutoIncrement, {inc_field: 'cart_id'});
+
+module.exports = mongoose.model('Cart', cartSchema);
