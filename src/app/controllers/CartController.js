6888c6da31d0ed608d8453960b8c5f02d3bf8898
dmanh <dmanh6302@gmail.com>
2023-04-06 11:05:50 +0700

    add product to user's cart on db

--- src/app/controllers/CartController.js
+++ src/app/controllers/CartController.js
@@ -4,45 +4,68 @@ const { multipleMongooseToObject, } = require('../../util/mongoose');
 class CartController {
 
     // [GET] /cart
-    index(req, res, next) {
-        let cartQuantity
-        if (req.session.cart) {
-            cartQuantity = req.session.cart.reduce((total, item) => total + item.quantity, 0);
-            const productSlugs = req.session.cart.map(item => item.productSlug);
-            Product.find({ slug: { $in: productSlugs } })
-                .then(products => {
-                    const cartItems = products.map(product => {
-                        const cartItem = req.session.cart.find(item => item.productSlug === product.slug);
+    async index(req, res, next) {
+        let cartQuantity = 0;
+        let cartItems = [];
+
+        if (req.session.currentUser) {
+            const accountId = req.session.currentUser.accountId;
+            try {
+                const userCart = await Cart.findOne({ accountId });
+                console.log('usercart:', userCart)
+                if (userCart && userCart.items.length > 0) {
+                    const productSlugs = userCart.items.map((item) => item.productSlug);
+                    const products = await Product.find({ slug: { $in: productSlugs } });
+                    cartItems = userCart.items.map((item) => {
+                        const product = products.find((p) => p.slug === item.productSlug);
                         return {
                             name: product.name,
                             price: product.price,
-                            quantity: cartItem.quantity,
-                            totalCost: cartItem.quantity * product.price,
+                            quantity: item.quantity,
+                            totalCost: item.quantity * product.price,
                             imageId: product.imageId,
-                            slug: product.slug
-                        }
+                            slug: product.slug,
+                        };
                     });
-                    res.render('cart/index', {
-                        items: cartItems,
-                        cartQuantity,
-                    });
-                })
-                .catch(err => {
-                    console.error(err);
-                    // Handle errors
+                    cartQuantity = cartItems.reduce((total, item) => total + item.quantity, 0);
+                }
+                renderCart(cartItems, cartQuantity);
+            } catch (err) {
+                console.log(err);
+                res.status(500).send('Internal Server Error');
+                return;
+            }
+        } else if (req.session.cart) {
+            const productSlugs = req.session.cart.map((item) => item.productSlug);
+            try {
+                const products = await Product.find({ slug: { $in: productSlugs } });
+                cartItems = products.map((product) => {
+                    const cartItem = req.session.cart.find((item) => item.productSlug === product.slug);
+                    return {
+                        name: product.name,
+                        price: product.price,
+                        quantity: cartItem.quantity,
+                        totalCost: cartItem.quantity * product.price,
+                        imageId: product.imageId,
+                        slug: product.slug,
+                    };
                 });
+                cartQuantity = cartItems.reduce((total, item) => total + item.quantity, 0);
+                renderCart(cartItems, cartQuantity);
+            } catch (err) {
+                console.error(err);
+                // Handle errors
+            }
+        } else {
+            renderCart(cartItems, cartQuantity);
         }
 
-        else {
-            cartQuantity = 0
-
+        function renderCart(items, quantity) {
             res.render('cart/index', {
-                cartQuantity,
+                items,
+                cartQuantity: quantity,
             });
         }
-
-
-        // console.log(req.session.cart);
     }
 
     // [POST] /add/
@@ -54,24 +77,71 @@ class CartController {
             quantity,
         }
 
-        // Add item to the cart on current session
-        if (!req.session.cart) {
-            // If the cart doesn't exist on the session yet, create it
-            req.session.cart = [];
-        }
-        // Check if the product is already in the cart
-        const existingItemIndex = req.session.cart.findIndex(item => item.productSlug === productSlug);
-        if (existingItemIndex !== -1) {
+        // ON LOGGED IN
+        if (req.session.currentUser) {
+            const accountId = req.session.currentUser.accountId;
 
-            req.session.cart[existingItemIndex].quantity += quantity;
-        } else {
-            // If the product is not in the cart yet, add it as a new item
-            req.session.cart.push(cartItem);
+            // Find user's cart in the database
+            Cart.findOne({ accountId })
+                .then((userCart) => {
+                    if (!userCart) {
+                        // If user's cart doesn't exist in the database yet, create it
+                        userCart = new Cart({
+                            accountId,
+                            items: [],
+                        });
+                    }
+
+                    // Check if the product is already in the cart
+                    const existingItemIndex = userCart.items.findIndex(item => item.productSlug === productSlug);
+
+                    if (existingItemIndex !== -1) {
+                        // If the product is already in the cart, update the quantity
+                        userCart.items[existingItemIndex].quantity += quantity;
+                    } else {
+                        // If the product is not in the cart yet, add it as a new item
+                        userCart.items.push(cartItem);
+                    }
+
+                    // Save user's updated cart in the database
+                    userCart.save()
+                        .then(() => {
+                            // Return the current cart quantity to the client
+                            const currentQuantity = userCart.items.reduce((total, item) => total + item.quantity, 0);
+                            res.json({ success: true, currentQuantity });
+                        })
+                        .catch(err => {
+                            console.error(err);
+                            // Handle errors
+                        });
+                })
+                .catch(err => {
+                    console.error(err);
+                    // Handle errors
+                });
         }
-        req.session.save()
-        const currentQuantity = req.session.cart.reduce((total, item) => total + item.quantity, 0);
-        res.json({ success: true, currentQuantity });
+        // ON SESSION
+        else {
+            // Add item to the cart on current session
+            if (!req.session.cart) {
+                // If the cart doesn't exist on the session yet, create it
+                req.session.cart = [];
+            }
+            // Check if the product is already in the cart
+            const existingItemIndex = req.session.cart.findIndex(item => item.productSlug === productSlug);
+            if (existingItemIndex !== -1) {
+
+                req.session.cart[existingItemIndex].quantity += quantity;
+            } else {
+                // If the product is not in the cart yet, add it as a new item
+                req.session.cart.push(cartItem);
+            }
+            req.session.save()
+            const currentQuantity = req.session.cart.reduce((total, item) => total + item.quantity, 0);
+            res.json({ success: true, currentQuantity });
+        }
     }
+
 
     // [POST] /update-cart-quantity
     updateCart(req, res, next) {
