1c27c311f2f8215d9093f50cebe08640e400691f
dmanh <dmanh6302@gmail.com>
2023-04-07 02:03:28 +0700

    fix cookies bugs

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -1,4 +1,5 @@
 const Product = require('../models/Product');
+const Order = require('../models/Order');
 const { multipleMongooseToObject, } = require('../../util/mongoose');
 class MeController {
     // [GET] /me/account
@@ -7,7 +8,16 @@ class MeController {
     }
 
     orders(req, res, next) {
-        res.end('orders page')
+        const userId = req.session.currentUser.accountId;
+        Order.find({ created_by: userId })
+            .then(orders => {
+                res.render('me/orders', { orders: multipleMongooseToObject(orders) });
+            })
+            .catch(error => {
+                console.log(error);
+                const errorMessage = 'Error retrieving orders';
+                res.status(500).render('error', { message: errorMessage });
+            });
     }
 
 }
