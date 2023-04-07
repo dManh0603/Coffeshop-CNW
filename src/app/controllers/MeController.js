d1f284c08949e2860004505f3ea7ef528193064e
dmanh <dmanh6302@gmail.com>
2023-04-07 08:49:59 +0700

    load orders from db

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -10,8 +10,20 @@ class MeController {
     orders(req, res, next) {
         const userId = req.session.currentUser.accountId;
         Order.find({ created_by: userId })
+            .select('order_id phone total isPaid firstname lastname')
             .then(orders => {
-                res.render('me/orders', { orders: multipleMongooseToObject(orders) });
+                const modifiedOrders = orders.map(order => {
+                    const fullname = order.firstname + ' ' + order.lastname;
+                    return {
+                        order_id: order.order_id,
+                        phone: order.phone,
+                        total: order.total,
+                        isPaid: order.isPaid,
+                        fullname: fullname
+                    };
+                });
+                console.log(multipleMongooseToObject(modifiedOrders));
+                res.render('me/orders', { orders: multipleMongooseToObject(modifiedOrders) });
             })
             .catch(error => {
                 console.log(error);
@@ -19,6 +31,7 @@ class MeController {
                 res.status(500).render('error', { message: errorMessage });
             });
     }
+
 
 }
 
